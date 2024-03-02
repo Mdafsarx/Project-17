55ad8b3d5bac8ab1631119aeeb1c1d72731e02d4
Mdafsarx <mdafsar99009@gmail.com>
2024-03-02 17:27:45 +0600

    update the website

--- config.js
+++ config.js
@@ -1,108 +1,112 @@
+// buy now 
+getId('buyNow').addEventListener('click',()=>{
+    scrollBy(0,910)
+})
 
-
-const Api=async (userSearched='iphone')=>{
-    const url=`https://openapi.programming-hero.com/api/phones?search=${userSearched}`
-    const response=await fetch(url)
-    const Data=await response.json()
-    const data=Data.data;
-    Display(data,seeMoreStatus)
-}
-Api()
-// user search
+// search
 function search(){
-    loading(true)
-    const inputField=getId('inputField').value ;
-    Api(inputField)
-}
-
-let seeMoreStatus=false;
-
-// display ////
-function Display(DATA,seeMoreStatus,dataLoad){
+    const inputField=getId('inputField').value 
+    dataLoading(false)
+    ApiDataLoad(inputField,isMore)
     
-    // phone container
+}
+async function ApiDataLoad(userSearch,isMore){
+    const url=`https://openapi.programming-hero.com/api/phones?search=${userSearch}`;
+    const res=await fetch(url)
+    const data=await res.json()
+    const Data=data.data;
+    display(Data,isMore)
+}
+ApiDataLoad('z')
+
+function display(phone,isMore){
 const phoneContainer=getId('cardContainer')
 phoneContainer.innerHTML=''
 
+if(phone.length===0){
+    const inputField=getId('inputField').value 
+getId('invalidSearch').innerText=inputField
+    getId('emptyMessage').classList.remove('hidden')
 
-
-
-// see more button Show and hide
-if(DATA.length>6){
-    getId('seeMore').classList.remove('hidden')
 }else{
-    getId('seeMore').classList.add('hidden')}
-
-// how much show in display
-if(!seeMoreStatus){
-    DATA=DATA.slice(0,6)
-}else{
-    DATA=DATA
+    getId('emptyMessage').classList.add('hidden')
+}
+if(isMore){
+    phone=phone
     getId('seeMore').classList.add('hidden')
+}else if(phone.length>6){phone=phone.slice(0,6)
+getId('seeMore').classList.remove('hidden')
 }
 
-    // single phone
-    for(const phone of DATA){
+phone.forEach(phn => {
+    // console.log(phn)
+    // create phone Card here
 const phoneCard=document.createElement('div')
-phoneCard.className='card bg-base-100 shadow-xl border-2 border-black'
+phoneCard.classList='card bg-base-100 shadow-xl border-2 border-black px-4 py-6'
 phoneCard.innerHTML=`
-
-<figure class="px-10 pt-10 ">
-<img src="${phone.image}" alt="Shoes" class="rounded-xl bg-[#0D6EFD0D] px-24 py-10" />
-</figure>
-<div class="card-body items-center text-center">
-<h2 class="card-title text-2xl font-bold">${phone.phone_name}</h2>
-<p class="font-bold text-xl"></p>
-<button class="btn bg-[#0D6EFD] text-white font-bold DetailsBtn" onclick="Details('${phone.slug}')">Show Details</button>
-</div>
-
+<figure><img src="${phn.image}" alt="Shoes" /></figure>
+        <div class="text-center py-3 space-y-2">
+          <h2 class="text-2xl font-bold">${phn.phone_name}</h2>
+          <p class="text-xl font-bold">Brand: ${phn.brand}</p>
+          <button class="btn bg-blue-600 text-white" onclick="Details('${phn.slug}')">Show Details</button>
+        </div>
 `
-phoneContainer.appendChild(phoneCard) }
-loading(false)
+phoneContainer.appendChild(phoneCard)});
+dataLoading(true)
+}
+
+
+// data loading
+
+function dataLoading(dataLoaded){
+    if(dataLoaded){
+        getId('dataLoad').classList.add('hidden')
+    }else{
+        getId('dataLoad').classList.remove('hidden')
+    }
+}
+
+// see more
+let isMore=false;
+function seeMore(){
+isMore=true
+search()
 
 }
 
 
-// see more details btn
+// details Show
 
 function Details(phoneId){
-    const url=`https://openapi.programming-hero.com/api/phone/${phoneId}`
-    fetch(url)
-    .then(response=>response.json())
-    .then(Data=>{
-const data=Data.data;
-console.log(data)
-D.showModal()
-// modal create
-const Modal=getId('D')
-Modal.innerHTML=`
+
+const url=`https://openapi.programming-hero.com/api/phone/${phoneId}`
+fetch(url)
+.then(res=>res.json())
+.then(data=>{
+console.log(data.data)
+const modal=getId('D')
+
+modal.innerHTML=`
 <div class="modal-box">
-        <div class="flex justify-center items-center"> <img src="${data.image}" alt="" class="bg-[#0D6EFD0D] px-20 py-10"></div>
-          <div class='flex flex-col items-center justify-center '>
-          <p class="text-3xl font-bold py-4">${data.name}</p>
-          <p class="text-base font-bold text-balance">Storage: ${data?.mainFeatures?.storage||'00'}</p>
-          <p class="text-base font-bold text-balance">Storage: ${data?.releaseDate||'Information not available'}</p>
-          <div class="modal-action flex justify-center">
-            <form method="dialog">
-              <!-- if there is a button in form, it will close the modal -->
-              <button class="btn">Close</button>
-            </form>
-          </div>
-          </div>
-        </div>
-`   })}
+    <div class="flex justify-center items-center"><img src='${data.data.image}' class="bg-cyan-300 px-16 py-10 rounded-2xl"></div>
+    <div class='pl-5 mt-2'>
+    <p class="text-2xl font-bold">${data.data.name}</p>
+    <p class="text-xl font-semibold">${data.data?.releaseDate}</p>
+    <p class="text-xl font-semibold">Memory: ${data.data?.mainFeatures?.memory}</p>
+    <p class="text-xl font-semibold">sensors: ${data.data?.mainFeatures?.sensors[0]}</p>
+    <p class="text-xl font-semibold">Bluetooth: ${data.data.others?.Bluetooth}</p>
+    
+    </div>
 
+    <div class="modal-action">
+      <form method="dialog">
+        <!-- if there is a button in form, it will close the modal -->
+        <button class="btn">Close</button>
+      </form>
+    </div>
+  </div>
+`
 
-// see more btn
- function seeMore(){
-seeMoreStatus=true;
-search()
- }
-
-
-// data loading 
-function loading(dataLoad){
-if(dataLoad){
-    getId('dataLoad').classList.remove('hidden')
-}else{
-    getId('dataLoad').classList.add('hidden')}}
+    D.showModal()
+})
+}
